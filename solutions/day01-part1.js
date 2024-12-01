36e8f5db9b4e5191855f62fef9f618b791bb7392
Tyler Smith <tylersmith4596@gmail.com>
2024-12-01 03:15:38 -0800

    refactor: remove unnecessary whitespace in day01-part1.js

--- solutions/day01-part1.js
+++ solutions/day01-part1.js
@@ -6,7 +6,7 @@ function parseInput(filePath) {
   const data = fs.readFileSync(filePath, 'utf-8').trim();
   const left = [];
   const right = [];
-  
+
   data.split('\n').forEach(line => {
     const [l, r] = line.split(/\s+/).map(Number);
     left.push(l);
